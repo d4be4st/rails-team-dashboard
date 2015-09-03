069418912c09462c4d934bfa5b696f3892d58911
Stjepan Hadjic <d4be4st@gmail.com>
2015-09-03 12:20:47 +0200

    rewrote uptime robot to use account details

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Rails team dashboard
 
 This is the dashboard that displays information usefull for [Infinum's](https://infinum.co) rails team.
+
+# Prerequisites
+
+```
+npm install -g node-gyp
+```
--- jobs/uptime_robot.job.js
+++ jobs/uptime_robot.job.js
@@ -1,58 +1,42 @@
 var request = require('request');
 
 var token = process.env.UPTIME_ROBOT_API_KEY;
-var url = 'http://api.uptimerobot.com/getMonitors?noJsonCallback=1&customUptimeRatio=30&format=json&apiKey=' + token;
+var url = 'http://http://api.uptimerobot.com/getAccountDetails?noJsonCallback=1&format=json&apiKey=' + token;
 
 var updateMonitors = function() {
-  var monitors = [];
-  fetchMonitors(0, monitors);
+  fetchAccountDetail();
 };
 
-function fetchMonitors(offset, monitors) {
+function fetchAccountDetail(offset, monitors) {
   request(
-    url + '&offset=' + offset,
+    url,
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        var monitorList = JSON.parse(body);
-        monitors = monitors.concat(monitorList.monitors.monitor);
-        var total = parseInt(monitorList.total);
-
-        if (monitors < total) {
-          fetchMonitors(offset + 50, monitors);
-          return;
-        }
-
-        var upMonitors = 0;
-        var downMonitors = 0;
-
-        for (var i = 0; i < monitors.length; i++) {
-          if (monitors[i].status === '2') {
-            upMonitors++;
-          }
-          else if (monitors[i].status === '8' || monitors[i].status === '9') {
-            downMonitors++;
-          }
-        }
+        var accountDetail = JSON.parse(body);
+        var upMonitors = parseInt(accountDetail.account.upMonitors);
+        var downMonitors = parseInt(accountDetail.account.downMonitors);
+        var pausedMonitors = parseInt(accountDetail.account.pausedMonitors);
+        var totalMonitors = upMonitors + downMonitors + pausedMonitors;
 
         send_event(
           'up_monitors',
           {
             value: upMonitors,
-            max: upMonitors + downMonitors
+            max: totalMonitors
           }
         );
         send_event(
           'down_monitors',
           {
             value: downMonitors,
-            max: upMonitors + downMonitors
+            max: totalMonitors
           }
         );
         send_event(
           'paused_monitors',
           {
-            value: total - (downMonitors + upMonitors),
-            max: total
+            value: pausedMonitors,
+            max: totalMonitors
           }
         );
       }
